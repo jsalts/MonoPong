b31a5b74790543eca1cddb49283f9bf203fe3ca8
Jeremiah <phase.variance@gmail.com>
2019-11-20 10:29:55 -0500

    Created Class for paddles. Updated rest of code to use paddle objects.

--- MonoPong/Game1.cs
+++ MonoPong/Game1.cs
@@ -7,6 +7,41 @@ namespace MonoPong
     /// <summary>
     /// This is the main type for your game.
     /// </summary>
+    public class Paddle
+    {
+        private Vector2 paddlePosition;
+
+        public Paddle(int x, int y)
+        {
+            this.paddlePosition = new Vector2(x, y);
+        }
+        public float GetX()
+        {
+            return paddlePosition.X;
+        }
+        public float GetY()
+        {
+            return paddlePosition.Y;
+        }
+        public Vector2 GetPosition()
+        {
+            return paddlePosition;
+        }
+        public void MovePaddle(float speed)
+        {
+            paddlePosition.Y += speed;
+            if (paddlePosition.Y < 0)
+                paddlePosition.Y = 0;
+            if (paddlePosition.Y + 100 > 480)
+                paddlePosition.Y = 380;
+        }
+        public Vector2 PaddlePosition
+        {
+            get { return paddlePosition; }
+            set { PaddlePosition = value;  }
+        }
+    }
+
     public class Game1 : Game
     {
         private float _gameSpeed = 3f;
@@ -17,17 +52,17 @@ namespace MonoPong
         Texture2D _ballTexture;
         Texture2D _paddleTexture;
         Vector2 _ballPosition;
-        Vector2 _playerPaddlePosition;
-        Vector2 _aiPaddlePosition;
-        int _direction;
+        Vector2 _ballSpeed;
+        Paddle _playerPaddle;
+        Paddle _aiPaddle;
 
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
             _ballPosition = new Vector2(50, 100);
-            _playerPaddlePosition = new Vector2(30, 20);
-            _aiPaddlePosition = new Vector2(750, 70);
+            _playerPaddle = new Paddle(30,20);
+            _aiPaddle = new Paddle(750, 70);
         }
 
         /// <summary>
@@ -57,7 +92,7 @@ namespace MonoPong
 
             base.Initialize();
 
-            _direction = 1;
+            _ballSpeed = new Vector2(1, 0);
         }
 
         /// <summary>
@@ -100,26 +135,23 @@ namespace MonoPong
         private void DetermineBallPosition()
         {
             //Move ball
-            if (_direction == 1)
-                _ballPosition.X += 1 * _gameSpeed;
-            else
-                _ballPosition.X -= 1 * _gameSpeed;
+            _ballPosition.X += _ballSpeed.X * _gameSpeed;
 
             //Check for right player paddle collision
-            if (_ballPosition.X+100 == _aiPaddlePosition.X)
+            if (_ballPosition.X+100 == _aiPaddle.GetX())
             {
-                if (_ballPosition.Y > _aiPaddlePosition.Y && _ballPosition.Y < _aiPaddlePosition.Y + 100)
-                    _direction = 0;
-                if (_ballPosition.Y + 100 > _aiPaddlePosition.Y && _ballPosition.Y + 100 < _aiPaddlePosition.Y + 100)
-                    _direction = 0;
+                if (_ballPosition.Y > _aiPaddle.GetY() && _ballPosition.Y < _aiPaddle.GetY() + 100)
+                    _ballSpeed.X = -1;
+                if (_ballPosition.Y + 100 > _aiPaddle.GetY() && _ballPosition.Y + 100 < _aiPaddle.GetY() + 100)
+                    _ballSpeed.X = -1;
             }
             //Check for left player paddle collision
-            if (_ballPosition.X == _playerPaddlePosition.X + 20)
+            if (_ballPosition.X == _playerPaddle.GetX() + 20)
             {
-                if (_ballPosition.Y > _playerPaddlePosition.Y && _ballPosition.Y < _playerPaddlePosition.Y + 100)
-                    _direction = 1;
-                if (_ballPosition.Y + 100 > _playerPaddlePosition.Y && _ballPosition.Y + 100 < _playerPaddlePosition.Y + 100)
-                    _direction = 1;
+                if (_ballPosition.Y > _playerPaddle.GetY() && _ballPosition.Y < _playerPaddle.GetY() + 100)
+                    _ballSpeed.X = 1;
+                if (_ballPosition.Y + 100 > _playerPaddle.GetY() && _ballPosition.Y + 100 < _playerPaddle.GetY() + 100)
+                    _ballSpeed.X = 1;
             }
 
             //Check for game over
@@ -135,14 +167,15 @@ namespace MonoPong
                 Exit();
             
             if (Keyboard.GetState().IsKeyDown(Keys.Down))
-                _aiPaddlePosition.Y += 1 * _gameSpeed;
+                //_aiPaddlePosition.Y += 1 * _gameSpeed;
+                _aiPaddle.MovePaddle(1 * _gameSpeed);
             if (Keyboard.GetState().IsKeyDown(Keys.Up))
-                _aiPaddlePosition.Y -= 1 * _gameSpeed;
-            
+                //_aiPaddlePosition.Y -= 1 * _gameSpeed;
+                _aiPaddle.MovePaddle(-1 * _gameSpeed);
             if (Keyboard.GetState().IsKeyDown(Keys.O) || Keyboard.GetState().IsKeyDown(Keys.S))
-                _playerPaddlePosition.Y += 1 * _gameSpeed;
+                _playerPaddle.MovePaddle(1 * _gameSpeed);
             if (Keyboard.GetState().IsKeyDown(Keys.OemComma) || Keyboard.GetState().IsKeyDown(Keys.W))
-                _playerPaddlePosition.Y -= 1 * _gameSpeed;
+                _playerPaddle.MovePaddle(-1 * _gameSpeed);
         }
 
         /// <summary>
@@ -158,10 +191,10 @@ namespace MonoPong
             _ballSprite.Draw(_ballTexture, _ballPosition, Color.White);
             _ballSprite.End();
             _playerPaddleSprite.Begin();
-            _playerPaddleSprite.Draw(_paddleTexture, _playerPaddlePosition, Color.White);
+            _playerPaddleSprite.Draw(_paddleTexture, _playerPaddle.GetPosition(), Color.White);
             _playerPaddleSprite.End();
             _aiPaddleSprite.Begin();
-            _aiPaddleSprite.Draw(_paddleTexture, _aiPaddlePosition, Color.White);
+            _aiPaddleSprite.Draw(_paddleTexture, _aiPaddle.GetPosition(), Color.White);
             _aiPaddleSprite.End();
             base.Draw(gameTime);
         }
