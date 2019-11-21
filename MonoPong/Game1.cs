f2286be7f561cd1b436d795e2a80bca373353555
Jamie Salts <jamie.salts@riverbed.com>
2019-11-20 20:54:53 -0500

    Pull Paddle out

--- MonoPong/Game1.cs
+++ MonoPong/Game1.cs
@@ -4,49 +4,11 @@ using Microsoft.Xna.Framework.Input;
 
 namespace MonoPong
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
-    public class Paddle
-    {
-        private Vector2 paddlePosition;
-
-        public Paddle(int x, int y)
-        {
-            this.paddlePosition = new Vector2(x, y);
-        }
-        public float GetX()
-        {
-            return paddlePosition.X;
-        }
-        public float GetY()
-        {
-            return paddlePosition.Y;
-        }
-        public Vector2 GetPosition()
-        {
-            return paddlePosition;
-        }
-        public void MovePaddle(float speed)
-        {
-            paddlePosition.Y += speed;
-            if (paddlePosition.Y < 0)
-                paddlePosition.Y = 0;
-            if (paddlePosition.Y + 100 > 480)
-                paddlePosition.Y = 380;
-        }
-        public Vector2 PaddlePosition
-        {
-            get { return paddlePosition; }
-            set { PaddlePosition = value;  }
-        }
-    }
-
     public class Game1 : Game
     {
-        private float _gameSpeed = 2f;
+        private float _gameSpeed = 10f;
 
-        private int _ballSize = 50;
+        private int _ballSize = 25;
         private int _boost1Width = 12;
         private int _boost2Width = 10;
         private int _boost1Height = 50;
---
+++ MonoPong/Player/Paddle.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace MonoPong
+{
+    /// <summary>
+    /// This is the main type for your game.
+    /// </summary>
+    public class Paddle
+    {
+        private Vector2 paddlePosition;
+
+        public Paddle(int x, int y)
+        {
+            this.paddlePosition = new Vector2(x, y);
+        }
+        public float GetX()
+        {
+            return paddlePosition.X;
+        }
+        public float GetY()
+        {
+            return paddlePosition.Y;
+        }
+        public Vector2 GetPosition()
+        {
+            return paddlePosition;
+        }
+        public void MovePaddle(float speed)
+        {
+            paddlePosition.Y += speed;
+            if (paddlePosition.Y < 0)
+                paddlePosition.Y = 0;
+            if (paddlePosition.Y + 100 > 480)
+                paddlePosition.Y = 380;
+        }
+        public Vector2 PaddlePosition
+        {
+            get { return paddlePosition; }
+            set { PaddlePosition = value;  }
+        }
+    }
+}
